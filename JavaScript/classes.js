5f37e6abd259d91a59077925db80269aac8c73dc
Patrick Mitchell (RIT Student) <prm5983@ad.rit.edu>
2019-12-03 15:14:50 -0500

    Fixed Tile class

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -1,12 +1,12 @@
-class Tile extends PIXI.Sprite {
+class Tile extends PIXI.Sprite.from {
     constructor(x = 0, y = 0, canTraverse = true, tint = 0xFFFFFF) {
         // set the anchor to the bottom left; helps with cartesian coordinates
+        super("Media/Tile-Sprite.png");
+
         this.anchor.set(0, 1);
         this.x = x;
         this.y = y;
         this.canTraverse = canTraverse;
-        this.image = canTraverse ? "Media/Tile-Sprite.png" : "";
-
-        super(PIXI.loader.resources[this.image].texture);
+        this.tint = tint;
     }
 }
