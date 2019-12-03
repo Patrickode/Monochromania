d20766ff8e01bc29447c856bcd4377b826fac4e5
Patrick <54603257+Patrickode@users.noreply.github.com>
2019-12-03 10:25:24 -0500

    Added PIXI cloudflare link, minor project initialization

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+// Make JavaScript sort of strongly typed
+"use strict";
+
+// Add Pixi to the canvas
+const app = new PIXI.Application(600, 600);
+document.body.appendChild(app.view);
+
+// constants
+const sceneWidth = app.view.width;
+const sceneHeight = app.view.height;
