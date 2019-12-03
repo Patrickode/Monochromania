6dd40ecaffead55d11bcd1760a26dfae626623db
Patrick Mitchell (RIT Student) <prm5983@ad.rit.edu>
2019-12-03 15:19:53 -0500

    Tweaked tile class and added comments

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -1,12 +1,14 @@
-class Tile extends PIXI.Sprite.from {
+class Tile extends PIXI.Sprite {
     constructor(x = 0, y = 0, canTraverse = true, tint = 0xFFFFFF) {
-        // set the anchor to the bottom left; helps with cartesian coordinates
-        super("Media/Tile-Sprite.png");
-
-        this.anchor.set(0, 1);
+        super(PIXI.loader.resources["Media/Tile-Sprite.png"].texture);
+        
+        this.anchor.set(0, 1);          //set the anchor to the bottom left; helps with cartesian coordinates
         this.x = x;
         this.y = y;
-        this.canTraverse = canTraverse;
-        this.tint = tint;
+        this.canTraverse = canTraverse; //can this tile be moved onto?
+        this.tint = tint;               //since Tiles are white, tint = the color of the tile
+
+        // If the tile isn't traversable, it shouldn't be visible either
+        this.visible = canTraverse;
     }
 }
