46eb587b78776c91ac907aeebdb562f45a52ca35
Patrick <54603257+Patrickode@users.noreply.github.com>
2019-12-03 10:36:04 -0500

    Added Brio to canvas (have to resize later)

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -8,3 +8,12 @@ document.body.appendChild(app.view);
 // constants
 const sceneWidth = app.view.width;
 const sceneHeight = app.view.height;
+
+// Assign player sprite image, set anchor to center of sprite
+let player = new PIXI.Sprite.from("Media/Brio-Sprite.png")
+player.anchor.set(0.5);
+
+// Place player on stage in the middle
+player.x = sceneWidth / 2;
+player.y = sceneHeight / 2;
+app.stage.addChild(player);
