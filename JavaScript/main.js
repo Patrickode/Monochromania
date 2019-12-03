78d001ef8c1ea800dc35e5057d8c48340e9c96ef
Patrick <54603257+Patrickode@users.noreply.github.com>
2019-12-03 10:57:14 -0500

    Added tileSize constant, made player 10 pixels less than tileSize (default 50)

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,11 +9,15 @@ document.body.appendChild(app.view);
 const sceneWidth = app.view.width;
 const sceneHeight = app.view.height;
 
+const tileSize = 50;
+
 // Assign player sprite image, set anchor to center of sprite
 let player = new PIXI.Sprite.from("Media/Brio-Sprite.png")
 player.anchor.set(0.5);
 
-// Place player on stage in the middle
+// Adjust player then place them on screen
+player.width = tileSize - 10;
+player.height = tileSize - 10;
 player.x = sceneWidth / 2;
 player.y = sceneHeight / 2;
 app.stage.addChild(player);
